3a1dc53cc0ef65c4868679a12f41694d5fd9175e
andrei-brando <albertbrando@hotmail.com>
2021-12-24 07:52:46 -0300

    refactor p/ componentizar aplicação

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,39 @@
-export function SideBar() {
-  // Complete aqui
+import './../styles/sidebar.scss';
+import { Button } from './Button';
+
+interface SideBarProps {
+  genres: GenreProps[];
+  selectedGenreId: number;
+  action: any;
+}
+
+interface GenreProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({action, genres, selectedGenreId}:SideBarProps) {
+  
+
+   return (
+     <>
+     <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => action(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+        </div>
+
+      </nav>
+     </>
+   );
 }
